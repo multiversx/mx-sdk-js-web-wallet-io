97444bc8e729783e5173534f94458c529ebc618e
Tudor Morar <tudor.morar@multiversx.com>
2025-01-13 11:34:27 +0200

    Increase max transactions to 100

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -2,6 +2,6 @@ export const IS_DEVELOPMENT =
   process.env.NODE_ENV === 'development' ||
   window?.location?.hostname === 'localhost';
 
-export const MAX_TRANSACTIONS = 50;
+export const MAX_TRANSACTIONS = 100;
 
 export const IS_TEST = process.env.NODE_ENV === 'test';
