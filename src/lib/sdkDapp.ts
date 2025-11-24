9d1219b8e17085f016f5ae9ca4f1502b03710ca1
Razvan Tomegea <razvantomegea@users.noreply.github.com>
2025-11-24 16:49:28 +0200

    Fixed imports from sdk-dapp (#64)
    * Update version to 3.2.1 and modify imports in sdkDapp.ts for better structure
    
    * Update CHANGELOG for version 3.2.1, correcting the pull request reference for sdk-dapp imports fix

--- src/lib/sdkDapp.ts
+++ src/lib/sdkDapp.ts
@@ -2,14 +2,12 @@ export {
   isStringBase64,
   decodeBase64,
   encodeToBase64
-} from '@multiversx/sdk-dapp/out/utils/decoders';
-export { isContract } from '@multiversx/sdk-dapp/out/utils/validation';
+} from '@multiversx/sdk-dapp/out/utils/decoders/base64Utils';
+export { isContract } from '@multiversx/sdk-dapp/out/utils/validation/isContract';
 export {
   decodeLoginToken,
   decodeNativeAuthToken
 } from '@multiversx/sdk-dapp/out/services/nativeAuth/helpers';
 
-export {
-  sanitizeCallbackUrl,
-  buildUrlParams
-} from '@multiversx/sdk-dapp/out/utils/window';
+export { sanitizeCallbackUrl } from '@multiversx/sdk-dapp/out/utils/window/sanitizeCallbackUrl';
+export { buildUrlParams } from '@multiversx/sdk-dapp/out/utils/window/buildUrlParams';
